d290b90b6867e593fe1f83301dbeb65300a83650
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-05-06 14:27:17 +0600

    100% commitment for developing world class tech company

--- hello.cpp
+++ hello.cpp
@@ -954,82 +954,135 @@ Below function is the main concepts
 //     std::cout << bmw.getName() << " is " << bmw.getName() << " ";
 // };
 
+// incapsulation
+// using std::string;
+// class Employees
+// {
+//     // attributes
+// private:
+//     int Age;
+//     int Height;
+//     string Name;
+//     string Company;
+
+//     // setting the setters and getters, to access the private class
+// public:
+//     void setAge(int age)
+//     {
+//         if (age >= 18)
+//             // setter
+//             age = Age;
+//     };
+//     int getAge()
+//     {
+//         // getter
+//         return Age;
+//     }
+//     void setHeight(int height)
+//     {
+//         height = Height;
+//     };
+//     int getHeight()
+//     {
+//         return Height;
+//     };
+
+//     void setName(string name)
+//     {
+//         name = Name;
+//     }
+//     string getName()
+//     {
+//         return Name;
+//     }
+
+//     void setCompany(string company)
+//     {
+//         company = Company;
+//     }
+//     string getCompany()
+//     {
+//         return Company;
+//     }
+
+//     // methods-> that will describe the objects
+// public:
+//     void yourdetails()
+//     {
+//         std::cout << Age << '\n';
+//         std::cout << Height << '\n';
+//         std::cout << Name << '\n';
+//         std::cout << Company << '\n';
+//     };
+//     // defining the constructors
+// public:
+//     Employees(int age, int height, string name, string company)
+//     {
+//         Age = age;
+//         Height = height;
+//         Name = name;
+//         Company = company;
+//     };
+// };
+// int main()
+// {
+//     // defining the objects, employee1 is the object that is classfied under the class "Employees";
+//     Employees employee1 = Employees(23, 34, "Tshewang", "Amazon");
+//     // employee1.age = 24;
+//     // employee1.height = 24;
+//     // employee1.name = "tshewang";  // employee1.age = 24;
+//     // employee1.height = 24;
+//     // employee1.name = "tshewang";
+//     // employee1.company = "Google";
+//     // employee1.company = "Google";
+//     // how to refactor this code, we can do that by using the constructors
+//     employee1.yourdetails();
+//     employee1.setAge(15);
+//     employee1.setCompany("google");
+//     employee1.setHeight(23);
+//     std::cout << employee1.getName() << "is " << employee1.getHeight() << "cm \n";
+//     std::cout
+//         << employee1.getName() << " is " << employee1.getAge() << "years old \n";
+//     std::cout << employee1.getName() << "is employee at " << employee1.getCompany();
+
+//     return 0;
+// }
+
+//  this the begining of new coding era.
+// abstractions-> means hiding the complex things in the background where only the simple part is being shown
+
+// declaring the class
 using std::string;
-class Employees
+class Fruits
 {
-    // attributes
-private:
-    int Age;
-    int Height;
+    // defining the user-datas inside the class
+public:
+    string Color;
     string Name;
-    string Company;
+    int Size;
 
-    // setting the setters and getters, to access the private class
-
-    void setAge(string age)
+    // methods( it is the functions that will describe objects)
+public:
+    void fruitsdetials()
     {
-        age = Age;
-    };
-    void getAge()
-    {
-        return Age;
+        std::cout << Name << "\n";
+        std::cout << Color << "\n";
+        std::cout << Size << "\n";
     }
-    void setHeight(string height)
+    // constructors
+public:
+    Fruits(string name, string color, int size)
     {
-        height = Height;
-    };
-    void getHeight()
-    {
-        return Height;
-    };
-
-    void setName(string name)
-    {
-        name = Name;
-    }
-    void getName()
-    {
-        return Name;
-    }
-
-    void setCompany(string company)
-    {
-        company = Company;
-    }
-    void getCompany()
-    {
-        return Company;
-    }
-
-    // methods-> that will describe the objects
-    void yourdetails()
-    {
-        std::cout << Age << '\n';
-        std::cout << Height << '\n';
-        std::cout << Name << '\n';
-        std::cout << Company << '\n';
-    };
-    // defining the constructors
-    Employees(int age, int height, string name, string company)
-    {
-        Age = age;
-        Height = height;
         Name = name;
-        Company = company;
-    };
+        Color = color;
+        Size = size;
+    }
+    // setter and getters
 };
+
 int main()
 {
-    // defining the objects, employee1 is the object that is classfied under the class "Employees";
-    Employees employee1 = Employees(23, 34, "Tshewang", "Amazon");
-    // employee1.age = 24;
-    // employee1.height = 24;
-    // employee1.name = "tshewang";
-    // employee1.company = "Google";
-    // how to refactor this code, we can do that by using the constructors
-    employee1.yourdetails();
-    employee1.setAge(30);
-    std::cout << employee1.getName() << " is " << employee1.getAge() << "years old \n";
-
-    return 0;
+    // defining the objects inside the main functions
+    Fruits groundFruits = Fruits("pineapple", "green", 23);
+    groundFruits.fruitsdetials();
 }
