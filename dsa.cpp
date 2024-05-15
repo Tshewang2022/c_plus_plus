0e4164432adc7d6e1ed22a6869d58a53d9ef39b4
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-05-15 22:14:22 +0600

    updated

--- dsa.cpp
+++ dsa.cpp
@@ -2,9 +2,36 @@
 
 /*
 list-> dealing with the arrays
-linked list-> dealing with dynamic arrays
+linked list-> dealing with dynamic arrays(will contains the data and its address)
+space and time complexity of the programming lanuages.
 */
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *head;
+void Print() {
+    // this is the functions
+};
+
+void Insert() {
+    // this is where all the dynamic memory allocations happens
+};
+
 int main()
 {
-    std::cout << "this will the next level coding with the pointers and memory allocations";
+    head = NULL; // emptylist;
+    printf("How many numbers? \n");
+    int n, i, x;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the number \n");
+        scanf("%d", &x);
+        Insert(x);
+        Print();
+    }
 }
