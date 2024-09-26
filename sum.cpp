82f314c0c6ddb53b243161690b6e92238700cc6e
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-09-26 16:33:34 +0600

    done with the welcome screen

--- sum.cpp
+++ sum.cpp
@@ -1,41 +1,81 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-class Solution
+// class Solution
+// {
+// public:
+//     std::vector<int> twoSum(std::vector<int> &nums, int target)
+//     {
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             for (int j = i + 1; j < nums.size(); j++)
+//             {
+//                 if (nums[i] + nums[j] == target)
+//                 {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+// int main()
+// {                                         // Single definition of main
+//     Solution solution;                    // Create an instance of Solution
+//     std::vector<int> nums = {1, 2, 3, 4}; // Use a vector
+//     int target = 4;
+
+//     std::vector<int> result = solution.twoSum(nums, target); // Call twoSum on the instance
+
+//     if (!result.empty())
+//     {
+//         std::cout << "Indices: [" << result[0] << ", " << result[1] << "]" << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "No solution found." << std::endl;
+//     }
+
+//     return 0;
+// }
+
+#include <vector>
+#include <iostream>
+
+int removeDuplicates(std::vector<int> &nums)
 {
-public:
-    std::vector<int> twoSum(std::vector<int> &nums, int target)
+    if (nums.empty())
     {
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j = i + 1; j < nums.size(); j++)
-            {
-                if (nums[i] + nums[j] == target)
-                {
-                    return {i, j};
-                }
-            }
-        }
-        return {};
+        return 0;
     }
-};
+
+    int k = 1; // Start k from 1 since the first element is always unique
+
+    for (int i = 1; i < nums.size(); ++i)
+    {
+        if (nums[i] != nums[i - 1])
+        {                      // Compare with the previous element
+            nums[k] = nums[i]; // Assign the unique element
+            k++;               // Move the pointer for unique elements
+        }
+    }
+
+    return k; // Return the number of unique elements
+}
 
 int main()
-{                                         // Single definition of main
-    Solution solution;                    // Create an instance of Solution
-    std::vector<int> nums = {1, 2, 3, 4}; // Use a vector
-    int target = 4;
+{
+    std::vector<int> nums = {1, 1, 2, 2, 3, 4};
+    int k = removeDuplicates(nums);
 
-    std::vector<int> result = solution.twoSum(nums, target); // Call twoSum on the instance
-
-    if (!result.empty())
+    std::cout << "Number of unique elements: " << k << std::endl;
+    std::cout << "Unique elements: ";
+    for (int i = 0; i < k; ++i)
     {
-        std::cout << "Indices: [" << result[0] << ", " << result[1] << "]" << std::endl;
+        std::cout << nums[i] << " ";
     }
-    else
-    {
-        std::cout << "No solution found." << std::endl;
-    }
+    std::cout << std::endl;
 
     return 0;
 }
