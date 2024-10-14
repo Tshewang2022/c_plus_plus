0ed86c241375149c233af93e0ca0a623c554d237
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-10-14 13:04:22 +0600

    grinded the oops concept, but need to look for the virtual functions

--- oop.cpp
+++ oop.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
 #include <string> // Include this header for std::string
-
-class Employee
+// abstraction concept, hiding the complex part of the class, and creating the instance of class with abstract class
+class abstractEmployee
 {
-public:
+    virtual void askPromotion() = 0;
+};
+class Employee : abstractEmployee
+{
+    // if we encapsulate the properties of this class using the private modifier, then we should set getters and setters function
+private:
     // Member variables
-    std::string name;
+
     int position;
     std::string company;
 
+protected:
+    std::string name;
+
+public:
+    void setName(std::string Name)
+    {
+        name = Name;
+    };
+    std::string getName()
+    {
+        return name;
+    };
+    void setPosition(int Position)
+    {
+        position = Position;
+    };
+    int getPosition()
+    {
+        return position;
+    };
+    void setCompany(std::string Company)
+    {
+        company = Company;
+    };
+    std::string getCompany()
+    {
+        return company;
+    };
     // Constructor
     Employee(std::string Name, int Position, std::string Company) // Use std::string here
     {
@@ -24,13 +57,45 @@ public:
         std::cout << "The position is " << position << std::endl;
         std::cout << "The company is " << company << std::endl;
     }
+    void askPromotion()
+    {
+        if (position < 1)
+        {
+            std::cout << "you have been promoted" << name << std::endl;
+        }
+        else
+        {
+            std::cout << "sorry no promotion for you" << name << std::endl;
+        }
+    }
 };
 
+// inheritance, the concept of having the inheriting the properties from the parent class is called as inheritance
+class Developer : Employee
+{
+public:
+    std::string favProgrammingLanguages;
+
+    // the constructor too will get inherited from the parent
+    Developer(std::string name, int position, std::string company, std::string FavProgramming) : Employee(name, position, company)
+    {
+        favProgrammingLanguages = FavProgramming;
+    }
+    void favOne()
+    {
+        std::cout << "this is " << name << " and i like " << favProgrammingLanguages << std::endl;
+    }
+};
 int main()
 {
     // Using the constructor to initialize the object
-    Employee staff1 = Employee("tshewang", 1, "technerds");
-    staff1.introduce();
+    // Employee staff1 = Employee("tshewang", 1, "technerds");
+    Developer staff3 = Developer("tshewang", 1, "tech_nerds", "c++");
+    staff3.favOne();
+    // staff1.introduce();
+    // staff1.setPosition(2);
+    // staff1.askPromotion();
+    // std::cout << "The name of the person is " << staff1.getName() << " the position is " << staff1.getPosition() << std::endl;
 
     return 0;
 }
