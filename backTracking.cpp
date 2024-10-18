3f79ca992c1c37705457edfb55e4d1e0134d6c28
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-10-18 11:35:52 +0600

    knapsack seems little odd the comprehend, but need little bit of deep work. nothing is in the world where i cant crack it

---
+++ backTracking.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+/* Backtracking use the brute force approach, where trying out all the possible solution for the particular problem*/
+int main()
+{
+    cout << "backtracking is trying out all the way of finding the solution" << endl;
+    return 0;
+}
---
+++ knapsackAlgo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm> // For std::max
+
+int main()
+{
+    int pt[5] = {0, 1, 2, 5, 6}; // Points/values of items
+    int wt[5] = {0, 2, 3, 4, 5}; // Weights of items
+
+    int m = 8;   // Maximum weight capacity
+    int n = 4;   // Number of items
+    int K[5][9]; // DP table
+
+    // Build the K table
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w = 0; w <= m; w++)
+        {
+            if (i == 0 || w == 0)
+            {
+                K[i][w] = 0; // Base case: no item or zero capacity
+            }
+            else if (wt[i] <= w)
+            {
+                K[i][w] = std::max(pt[i] + K[i - 1][w - wt[i]], K[i - 1][w]);
+            }
+            else
+            {
+                K[i][w] = K[i - 1][w]; // Cannot include the item
+            }
+        }
+    }
+
+    // Output the maximum value
+    std::cout << "The maximum value that can be obtained is: " << K[n][m] << std::endl;
+
+    return 0;
+}
