c9daa1a9754e780cf319534d2d0b4be01454307e
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-10-19 09:06:26 +0600

    nakamoto

--- mastering/day1Basics.cpp
+++ mastering/day1Basics.cpp
@@ -3,27 +3,79 @@ using namespace std;
 // follow the DRY method always
 /* oops concept in the c++, encapsulation, abstraction , inheritance , and polymorphism*/
 
+// class Pen
+// {
+// public:
+//     string Name;
+//     string Model;
+//  constructor
+//     Pen(string name, string model)
+//     {
+//         Name = name;
+//         Model = model;
+//     }
+//     void describe()
+//     {
+//         cout << "The name of the pen is " << Name << "and model is " << Model << endl;
+//     }
+
+// };
+// int main()
+// {
+//     // the ballPoint is repeating, should be removed and optimized the code
+//     Pen ballPoint("CelloGripper", "2016");
+//     // ballPoint.name = " CelloGripper";
+//     // ballPoint.model = "2016";
+//     ballPoint.describe();
+//     return 0;
+// }
+
+// ENCAPSULATION, making the class private
 class Pen
 {
-public:
-    string Name;
+private:
     string Model;
+
+protected:
+    string Name;
+
+public:
     Pen(string name, string model)
     {
         Name = name;
         Model = model;
     }
+
+public:
+    void setName(string name)
+    {
+        name = Name;
+    }
+    string getName()
+    {
+        return Name;
+    }
+    void setModel(string model)
+    {
+        Model = model;
+    }
+    string getModel()
+    {
+        return Model;
+    }
+
     void describe()
     {
         cout << "The name of the pen is " << Name << "and model is " << Model << endl;
     }
-
-    //  the constructor will be default
 };
 int main()
 {
     // the ballPoint is repeating, should be removed and optimized the code
     Pen ballPoint("CelloGripper", "2016");
+    cout << "The name of the person is " << ballPoint.getName() << endl;
+    ballPoint.setModel("2090");
+    cout << "The model of the pen is " << ballPoint.getModel() << endl;
     // ballPoint.name = " CelloGripper";
     // ballPoint.model = "2016";
     ballPoint.describe();
---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // swap function in c++
+    string x = "water";
+    string y = "potatos";
+
+    // temp variable
+    string temp;
+    temp = y;
+    y = x;
+    x = temp;
+    cout << "the value has been swaped successfully " << x << endl;
+    cout << "the value has been swaped successfully " << y << endl;
+}
