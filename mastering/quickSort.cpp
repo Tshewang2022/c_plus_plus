3c09b82eea561ecb1e2cdd58a178b9c0a785c745
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-10-21 11:12:52 +0600

    still need more practice and grinding

--- mastering/quickSort.cpp
+++ mastering/quickSort.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int partition(int A[], int l, int h)
 {
-    int pivot = A[l]; // Select the first element as the pivot
-    int i = l + 1;    // Start from the next element
-    int j = h;        // Start from the last element
+    int pivot = A[l];
+    int i = l + 1;
+    int j = h;
 
     while (true)
     {
-        // Increment i while A[i] is less than or equal to pivot and within bounds
         while (i <= h && A[i] <= pivot)
         {
             i++;
         }
-
-        // Decrement j while A[j] is greater than pivot and within bounds
         while (j > l && A[j] > pivot)
         {
             j--;
         }
-
-        // If i and j have crossed, we are done
         if (i >= j)
         {
             break;
         }
-
-        // Swap A[i] and A[j]
         swap(A[i], A[j]);
     }
 
-    // Place the pivot in its correct position
     swap(A[l], A[j]);
     return j; // Return the index of the pivot
+}
+
+void quickSort(int A[], int l, int h)
+{ // Accept the array as a parameter
+    if (l < h)
+    {
+        int j = partition(A, l, h); // Pass the array to partition
+        quickSort(A, l, j - 1);     // Corrected indices for recursion
+        quickSort(A, j + 1, h);
+    }
 }
 
 int main()
@@ -52,8 +55,8 @@ int main()
     }
     cout << endl;
 
-    int pivotIndex = partition(A, 0, 4);
-    cout << "Array after partitioning around the pivot: ";
+    quickSort(A, 0, 4); // Call quickSort to sort the array
+    cout << "Array after performing the quick sort: ";
     for (int i = 0; i < 5; i++)
     {
         cout << A[i] << " ";
