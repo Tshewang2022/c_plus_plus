e8690366ef7c7bb0af572a5c1ac541bc05e0ad52
Tshewang Gyaltshen <tshewanggyaltshen0@gmail.com>
2025-09-13 10:22:38 +0600

    using the stack and queue from the STL

--- basics/array.cpp
+++ basics/array.cpp
@@ -1,6 +1,10 @@
 // understanding the basic of computers is very much required
 // primitive data types , bool, int(short, long, double long=> signed || unsigned), char
 #include <iostream>
+#include <memory> // smart pointers to prevent memo leaks
+#include <queue>
+#include <stack>
+// #include <math>
 
 int main()
 {
@@ -12,23 +16,79 @@ int main()
     // sorting algo
     // searching algo
     // complex algo
-    int x = 6;
-    int y = 8;
-    int arry[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // int x = 6;
+    // int y = 8;
+    // int arry[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     // so now build the array from the given int
-    for (int i = 0; i < 8; i++)
-    {
-        // create a array from one to 8;
-        int arry[8];
-        arry[i] = i + 1;
-    }
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     // create a array from one to 8;
+    //     int arry[8];
+    //     arry[i] = i + 1;
+    // }
 
     // loop over to print the array
-    for (int i = 0; i < 8; i++)
-    {
-        std::cout << arry[i] << std::endl;
-    }
-    
-    std::cout << "Size of " << sizeof(arry) / sizeof(arry[0]) << std::endl;
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     std::cout << arry[i] << std::endl;
+    // }
+
+    // for the while loop structure
+    // int i = 1;
+    // while (i < 8)
+    // {
+    //     std::cout << "8 times print" << i++;
+    // }
+
+    // do
+    // {
+    //     std::cout << "Printed number 7" << std::endl;
+    // } while (i == 7);
+
+    // done with the control statement;
+
+    // pointers in cpp;
+    // we use * in pointers, ** for the double pointers;
+
+    // std::cout
+    //     << "Size of " << sizeof(arry) / sizeof(arry[0]) << std::endl;
     // endline
+    // pointers are being represented by
+    // * (deference op) and & op
+
+    // int x = 9;
+    // int *xy = &x;
+    // std::cout << *xy << '\n';
+    // this is the pointers
+
+    // parameter passing in c++
+    // pass by value, pass by pointers (int* prt) and pass by reference(&prt);
+    // smart pointer
+    // how do use queue in the cpp
+    std::queue<int> p;
+
+    // add element
+    p.push(1);
+    p.push(2);
+    p.push(3);
+
+    while (!p.empty())
+    {
+        std::cout << p.front() << std::endl;
+        p.pop();
+    }
+
+    std::stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    while (!s.empty())
+    {
+        std::cout << s.top() << std::endl;
+        s.pop();
+    }
+
+    return 0;
 }
