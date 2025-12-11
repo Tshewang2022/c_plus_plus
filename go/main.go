f5158c2eec478d1cfabf1f7c17694a7d9d53b8cf
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2025-12-11 00:35:50 -0600

    back to basics

--- go/main.go
+++ go/main.go
@@ -12,7 +12,25 @@
 package main
 
 import "fmt"
+// signed => can be both the positive and negative
+// unsigned => only the postive, that is best for the wallet integrations;
+// here the data type of all the language
+// numbers, string, boolean
 
+// operations=> basic math operations
+// loops 
+// 
+
+// how to declare the variables in the go;
+// var
+// const
+// := , this is the shorthand for declaring the variable in the go lang
+// NOTE** the variable declaration can be implicit or explicit, if explicit, go will assign the type naturally;
+
+
+// ALERT*** although, numbers belongs to the int, it itself have the different type, so it cannot be added, if of different type;
+// in simple, there is no type coercion ;
+// this will lead the memory overflow, if miscaculated;
 // main function => entry point
 func main() {
 	fmt.Println("Hello, World!")
