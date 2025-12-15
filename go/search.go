70de6bc6e601faea703ca06fe28fc4313e244aee
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2025-12-15 00:00:30 -0600

    updated the sorting algo

--- go/search.go
+++ go/search.go
@@ -1,66 +1,66 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+// import (
+// 	"fmt"
+// 	"math"
+// )
 
-// will be working on the searching algorithm;
-// linear search, binary search, and jump search;
-// how to streamline and break into the smaller pieces in the code;
+// // will be working on the searching algorithm;
+// // linear search, binary search, and jump search;
+// // how to streamline and break into the smaller pieces in the code;
 
-func main() {
-	// BinarySeach()
-	JumpSearch()
-}
+// func main() {
+// 	// BinarySeach()
+// 	JumpSearch()
+// }
 
-// implementation of binary search
-func BinarySeach() int {
-	sortedArr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	// binary search works, on the dividing into half;
-	l := 0
-	r := len(sortedArr) - 1
+// // implementation of binary search
+// func BinarySeach() int {
+// 	sortedArr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+// 	// binary search works, on the dividing into half;
+// 	l := 0
+// 	r := len(sortedArr) - 1
 
-	const target = 4
-	mid := (r + l) / 2
-	fmt.Println(mid, "printing mid")
+// 	const target = 4
+// 	mid := (r + l) / 2
+// 	fmt.Println(mid, "printing mid")
 
-	for l <= r {
-		if sortedArr[mid] == target {
-			return mid
-		} else if sortedArr[mid] < target {
-			l = mid + 1
-		} else {
-			r = mid - 1
-		}
+// 	for l <= r {
+// 		if sortedArr[mid] == target {
+// 			return mid
+// 		} else if sortedArr[mid] < target {
+// 			l = mid + 1
+// 		} else {
+// 			r = mid - 1
+// 		}
 
-	}
-	return -1
-}
+// 	}
+// 	return -1
+// }
 
-// implementation of jump search
-func JumpSearch() int {
-	sortedArr := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	l := len(sortedArr) - 1 // the length of the sorted arrays
-	prev := 0
-	fmt.Print(prev, "prev")
-	step := int(math.Sqrt(float64(sortedArr[l])))
-	const target = 8
+// // implementation of jump search
+// func JumpSearch() int {
+// 	sortedArr := []int{1, 2, 3, 4, 5, 6, 7, 8}
+// 	l := len(sortedArr) - 1 // the length of the sorted arrays
+// 	prev := 0
+// 	fmt.Print(prev, "prev")
+// 	step := int(math.Sqrt(float64(sortedArr[l])))
+// 	const target = 8
 
-	for sortedArr[int(math.Min(float64(step), float64(l)))-1] < target {
-		prev = step
-		step += int(math.Sqrt(float64(l)))
-		if prev >= l {
-			return -1
-		}
-	}
-	//  all is being played by this, logic, ==
-	for i := prev; i < int(math.Min(float64(step), float64(l))); i++ {
-		if sortedArr[i] == target {
-			return i
-		}
-	}
-	return -1
-}
+// 	for sortedArr[int(math.Min(float64(step), float64(l)))-1] < target {
+// 		prev = step
+// 		step += int(math.Sqrt(float64(l)))
+// 		if prev >= l {
+// 			return -1
+// 		}
+// 	}
+// 	//  all is being played by this, logic, ==
+// 	for i := prev; i < int(math.Min(float64(step), float64(l))); i++ {
+// 		if sortedArr[i] == target {
+// 			return i
+// 		}
+// 	}
+// 	return -1
+// }
 
-// never start something unless it has visible finish line;
+// // never start something unless it has visible finish line;
--- go/sorting.go
+++ go/sorting.go
@@ -1,1 +1,71 @@
-package go
+package main
+
+import "fmt"
+
+// sorting algorithm in go
+// bubble sort, selection sort, insertion sort, and merge sort, and heap sort;
+// the concept of the bubble sort;=> compare the element to the adjenct element, if it greater, swap the place;
+
+// sorting => swaping
+// searching => matching
+func bubbleSort() {
+	arr := []int{1, 29, 0, 7, 6, 2, 3}
+	// compare the element from one to adject;
+	l := len(arr) - 1
+	for i := 0; i < l; i++ {
+		for j := 0; j < l-i-1; j++ {
+
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	fmt.Println(arr)
+}
+
+// how does the insertion algo works;
+func insertionSort() {
+	arr := []int{1, 29, 0, 7, 6, 2, 3, 7}
+	l := len(arr)
+
+	for i := 1; i < l; i++ {
+		// 1 is already consider as the sorted;
+		key := arr[i] // element to be inserted
+		j := i - 1
+
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+
+	fmt.Println(arr, "print the array")
+}
+
+func selectionSort() []int {
+	// working on the selection sorting algo;
+	// find the smallest in the array, and swap it the first element;
+	arr := []int{1, 29, 0, 7, 6, 2, 3, 7}
+	l := len(arr)
+
+	for i := 0; i < l-1; i++ {
+		minIndex := i
+
+		for j := i + 1; j < l; j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
+	fmt.Print(arr)
+	return arr
+}
+
+func main() {
+	// bubbleSort()
+	// insertionSort()
+	selectionSort()
+
+}
